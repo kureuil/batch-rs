d25777e1b363ea6abd54661ebcb7a4f840f70df4
Louis Person <louis@person.guru>
2018-03-04 00:50:55 +0100

    Add test for priority queue

--- batch/src/rabbitmq.rs
+++ batch/src/rabbitmq.rs
@@ -544,3 +544,71 @@ impl QueueBuilder {
 pub fn queue(name: &str) -> QueueBuilder {
     QueueBuilder::new(name)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use task::Priority;
+
+    #[test]
+    fn priority_queue() {
+        let mut core = Core::new().unwrap();
+        let ex = "batch.tests.priorities";
+        let rk = "prioritised-hello";
+        let jobs = vec![
+            (Job::new("job-1", ex, rk, &[], None, 0), Priority::Normal),
+            (Job::new("job-2", ex, rk, &[], None, 0), Priority::Critical),
+            (Job::new("job-3", ex, rk, &[], None, 0), Priority::Trivial),
+            (Job::new("job-4", ex, rk, &[], None, 0), Priority::High),
+            (Job::new("job-5", ex, rk, &[], None, 0), Priority::Low),
+        ];
+        let expected = vec!["job-2", "job-4", "job-1", "job-5", "job-3"];
+
+        let conn_url = "amqp://localhost/%2f";
+        let exchanges = vec![exchange(ex).build()];
+        let queues = vec![
+            queue("tests.priorities")
+                .enable_priorities()
+                .bind(ex, rk)
+                .build(),
+        ];
+        let handle = core.handle();
+        let task = RabbitmqBroker::new_with_handle(
+            conn_url,
+            exchanges.clone(),
+            vec![],
+            handle.clone(),
+        ).and_then(|broker| {
+            let tasks = jobs.iter().map(move |&(ref job, ref priority)| {
+                let properties = BasicProperties {
+                    priority: Some(priority.to_u8()),
+                    ..Default::default()
+                };
+                broker.send(&job, properties)
+            });
+            future::join_all(tasks)
+        })
+            .and_then(move |_| RabbitmqBroker::new_with_handle(conn_url, exchanges, queues, handle))
+            .and_then(|broker| broker.recv())
+            .and_then(|consumer| {
+                future::loop_fn((consumer.into_future(), expected.clone()), |(f, order)| {
+                    f.map_err(|(e, _)| e)
+                        .and_then(move |(next, consumer)| {
+                            let head = order[0];
+                            let tail = order.into_iter().skip(1).collect::<Vec<_>>();
+                            let (uid, job) = next.unwrap();
+                            assert_eq!(job.name(), head);
+                            consumer.ack(uid).map(|_| (consumer, tail))
+                        })
+                        .and_then(|(consumer, order)| {
+                            if order.is_empty() {
+                                Ok(future::Loop::Break(()))
+                            } else {
+                                Ok(future::Loop::Continue((consumer.into_future(), order)))
+                            }
+                        })
+                })
+            });
+        core.run(task).unwrap();
+    }
+}
