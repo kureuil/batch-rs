99b90fcd5e4a756f026ac146083fa136444c3917
Louis Kureuil Person <louis@person.guru>
2018-10-11 20:29:44 +0200

    Add more doctests to batch-rabbitmq

--- batch-rabbitmq/src/exchange.rs
+++ batch-rabbitmq/src/exchange.rs
@@ -2,7 +2,7 @@
 ///
 /// Built-in types are represented as dedicated enum variants, and you can specify other exchange types using the
 /// `Kind::Custom` variant.
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Eq, PartialEq)]
 pub enum Kind {
     Direct,
     Fanout,
@@ -12,6 +12,7 @@ pub enum Kind {
 }
 
 impl Default for Kind {
+    /// The default exchange type is `direct`.
     fn default() -> Kind {
         Kind::Direct
     }
@@ -37,11 +38,31 @@ pub struct Builder {
 }
 
 impl Builder {
-    pub fn kind(&mut self, kind: Kind) -> &mut Self {
+    /// Sets the kind of the exchange that will be created.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use batch_rabbitmq::{Exchange, ExchangeKind};
+    ///
+    /// let kind = ExchangeKind::Topic;
+    /// let builder = Exchange::build("batch-exchange")
+    ///     .kind(kind);
+    /// ```
+    pub fn kind(mut self, kind: Kind) -> Self {
         self.kind = kind;
         self
     }
 
+    /// Consume the builder and create the exchange.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use batch_rabbitmq::Exchange;
+    ///
+    /// let exchange = Exchange::build("batch-example").finish();
+    /// ```
     pub fn finish(self) -> Exchange {
         Exchange {
             name: self.name,
@@ -58,25 +79,95 @@ pub struct Exchange {
 }
 
 impl Exchange {
-    pub fn new(name: String) -> Self {
+    /// Create a new `Exchange` with the default exchange type (direct).
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use batch_rabbitmq::Exchange;
+    ///
+    /// let exchange = Exchange::new("batch-example");
+    /// ```
+    pub fn new(name: impl Into<String>) -> Self {
         Exchange {
-            name,
-            kind: Kind::Direct,
+            name: name.into(),
+            kind: Kind::default(),
         }
     }
 
-    pub fn build(name: String) -> Builder {
+    /// Create a builder for a new `Exchange` named `name`.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use batch_rabbitmq::{Exchange};
+    ///
+    /// let builder = Exchange::build("batch-example");
+    /// ```
+    pub fn build(name: impl Into<String>) -> Builder {
         Builder {
-            name,
+            name: name.into(),
             kind: Kind::Direct,
         }
     }
 
+    /// The name of this exchange.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use batch_rabbitmq::Exchange;
+    ///
+    /// let exchange = Exchange::new("batch-example");
+    /// assert!(exchange.name() == "batch-example");
+    /// ```
     pub fn name(&self) -> &str {
         &self.name
     }
 
+    /// The type of this exchange.
+    ///
+    /// This method is named `kind` instead of `type` because it conflicts with the `type` keyword
+    /// of the Rust language.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use batch_rabbitmq::{Exchange, ExchangeKind};
+    ///
+    /// let exchange = Exchange::new("batch-example");
+    /// assert!(exchange.kind() == &ExchangeKind::Direct);
+    ///
+    /// let exchange = Exchange::build("batch-example")
+    ///     .kind(ExchangeKind::Custom("x-delay-exchange".into()))
+    ///     .finish();
+    /// assert!(exchange.kind() == &ExchangeKind::Custom("x-delay-exchange".into()));
+    /// ```
     pub fn kind(&self) -> &Kind {
         &self.kind
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_exchange_kind_as_ref_str() {
+        struct TestCase { kind: Kind, expected: &'static str }
+
+        let cases = vec![
+            TestCase { kind: Kind::Direct, expected: "direct" },
+            TestCase { kind: Kind::Fanout, expected: "fanout" },
+            TestCase { kind: Kind::Topic, expected: "topic" },
+            TestCase { kind: Kind::Headers, expected: "headers" },
+            TestCase { kind: Kind::Custom("".into()), expected: "" },
+            TestCase { kind: Kind::Custom("x-delay-exchange".into()), expected: "x-delay-exchange" },
+            TestCase { kind: Kind::Custom("direct".into()), expected: "direct" },
+        ];
+
+        for case in cases {
+            assert_eq!(case.kind.as_ref(), case.expected);
+        }
     }
 }
