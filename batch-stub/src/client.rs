kureuil/batch-rs

commit 57c15d3dfa333f4b0a365cca2e66d1c47a3e4445

Refactor the Queue trait to better accomodate with RabbitMQ

batch-stub/src/client.rs
@@ -80,7 +80,7 @@ impl Client {
         let inner = self.inner.lock().unwrap();
         inner
             .dispatches
-            .get(Q::NAME.into())
+            .get(Q::DESTINATION.into())
             .map(|v| v.len())
             .unwrap_or(0)
     }
@@ -171,7 +171,9 @@ mod tests {
     }
 
     impl batch::Queue for MaintenanceQueue {
-        const NAME: &'static str = "maintenance";
+        const SOURCE: &'static str = "maintenance";
+        
+        const DESTINATION: &'static str = "maintenance";
 
         type CallbacksIterator = std::vec::IntoIter<(
             &'static str,
@@ -194,7 +196,9 @@ mod tests {
     }
 
     impl batch::Queue for TranscodingQueue {
-        const NAME: &'static str = "transcoding";
+        const SOURCE: &'static str = "transcoding";
+        
+        const DESTINATION: &'static str = "transcoding";
 
         type CallbacksIterator = std::vec::IntoIter<(
             &'static str,

src/query.rs
@@ -1,5 +1,6 @@
 use failure::Error;
 use futures::{Async, Future, Poll};
+use log::debug;
 use std::marker::PhantomData;
 
 use dispatch::Dispatch;
@@ -128,10 +129,11 @@ where
     where
         C: Client,
     {
+        debug!("dispatch; job={} queue={}", J::NAME, Q::DESTINATION);
         let client = client.clone();
         SendDispatch {
             client,
-            state: DispatchState::Raw(Q::NAME, Some(self.job), Some(self.properties)),
+            state: DispatchState::Raw(Q::DESTINATION, Some(self.job), Some(self.properties)),
         }
     }
 }
@@ -163,6 +165,8 @@ where
     type Error = Error;
 
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        // FIXME: Refactor once NLL lands on stable, the weird dance to replace self.state
+        //        shouldn't be needed anymore.
         let state = match self.state {
             DispatchState::Raw(destination, ref mut job, ref mut props) => {
                 let dispatch = Dispatch::new(

batch-rabbitmq-codegen/src/queues.rs
@@ -241,7 +241,9 @@ impl ToTokens for Queue {
                 }
 
                 impl #export::Queue for #ident {
-                    const NAME: &'static str = #name;
+                    const SOURCE: &'static str = #name;
+                    
+                    const DESTINATION: &'static str = #exchange;
 
                     type CallbacksIterator = #export::Box<#export::Iterator<Item = (
                         &'static str,

src/queue.rs
@@ -9,8 +9,11 @@ use Factory;
 ///
 /// This trait is meant to be implemented by adapters to message brokers.
 pub trait Queue: Sized {
-    /// The name of this queue.
-    const NAME: &'static str;
+    /// The key used when publishing a job to this queue.
+    const SOURCE: &'static str;
+
+    /// The key used when consuming jobs from this queue.
+    const DESTINATION: &'static str;
 
     /// The return type of the `callbacks` method.
     type CallbacksIterator: Iterator<

src/worker.rs
@@ -126,7 +126,7 @@ where
     where
         Q: Queue,
     {
-        self.queues.insert(Q::NAME.into());
+        self.queues.insert(Q::SOURCE.into());
         for (job, callback) in Q::callbacks() {
             if let Some(previous) = self.callbacks.insert(job.into(), callback) {
                 if previous as fn(_, _) -> _ != callback as fn(_, _) -> _ {

commit 5b4f5b66cb2dc540a219e74f90fcfab62eeddfae

Fix tests that broke after 57c15d3

batch-stub/src/client.rs
@@ -249,6 +249,6 @@ mod tests {
     #[should_panic]
     fn test_creating_a_consumer_should_panic() {
         let mut client = Client::new();
-        let fut = client.to_consumer(vec![MaintenanceQueue::NAME, TranscodingQueue::NAME]);
+        let _ = client.to_consumer(vec![MaintenanceQueue::SOURCE, TranscodingQueue::SOURCE]);
     }
 }

src/query.rs
@@ -43,7 +43,9 @@ where
     /// # struct ExampleQueue;
     /// #
     /// # impl Queue for ExampleQueue {
-    /// #     const NAME: &'static str = "example-queue";
+    /// #     const SOURCE: &'static str = "example-queue";
+    /// #
+    /// #     const DESTINATION: &'static str = "example-queue";
     /// #
     /// #     type CallbacksIterator = Box<Iterator<
     /// #         Item = (
@@ -91,7 +93,9 @@ where
     /// # struct ExampleQueue;
     /// #
     /// # impl Queue for ExampleQueue {
-    /// #     const NAME: &'static str = "example-queue";
+    /// #     const SOURCE: &'static str = "example-queue";
+    /// #
+    /// #     const DESTINATION: &'static str = "example-queue";
     /// #
     /// #     type CallbacksIterator = Box<Iterator<
     /// #         Item = (