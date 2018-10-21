430a025e411e60868f70101dccb9d65dff1490e6
Louis Person <louis@person.guru>
2018-10-21 23:25:41 +0200

    Fix html_root_url in batch-test crate

--- batch-test/src/lib.rs
+++ batch-test/src/lib.rs
@@ -1,6 +1,6 @@
 //! Testing utilities for Batch.
 
-#![doc(html_root_url = "https://docs.rs/batch-stub/0.2.0")]
+#![doc(html_root_url = "https://docs.rs/batch-test/0.2.0")]
 #![deny(missing_debug_implementations)]
 #![deny(missing_docs)]
 
